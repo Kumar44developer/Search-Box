e4ca9a37dfd2bae5884aa78357beebf052adcbe9
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-12-12 09:13:45 +0530

    Added JavaScript variable to reference the cross icon element using querySelector

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ const container = document.querySelector(".container");
 const input = document.querySelector(".search");
 const button = document.getElementById("_button");
 const serachIcon = document.querySelector(".fa-search");
+const crossIcon = document.querySelector(".fa-times");
